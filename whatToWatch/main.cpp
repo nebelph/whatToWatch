c12ca0ad81dbd13daf697ad2eccdddcca34fa4b6
Artyom Rudakov <edu.rudakov@gmail.com>
2024-10-14 20:10:13 +0300

    Добавлены возможности для работы со списком (вращение, завершение, вывод). Добавлена проверка на ввод

--- whatToWatch/main.cpp
+++ whatToWatch/main.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <string> // to_string()
+#include <Windows.h> // Sleep()
+#include <math.h> // pow()
 
 class Activity {
 	std::string title_;
 public:
 	explicit Activity(std::string title) : title_(title) {}
-	std::string getTitle() const { return title_; }
+	virtual std::string getTitle() const { return title_; }
 
 };
 
-class Movie : public Activity {
-	unsigned year_;
-public:
-	Movie(std::string title, unsigned year) : Activity(title), year_(year) {}
-};
-
-class Series : public Activity {
-	unsigned year_;
-public:
-	Series(std::string title, unsigned year) : Activity(title), year_(year) {}
-};
+//class Movie : public Activity {
+//	unsigned year_;
+//public:
+//	Movie(std::string title, unsigned year) : Activity(title), year_(year) {}
+//	std::string getTitle() const override {
+//		return Activity::getTitle() + ", " + std::to_string(year_);
+//	}
+//};
+//
+//class Series : public Activity {
+//	unsigned year_;
+//public:
+//	Series(std::string title, unsigned year) : Activity(title), year_(year) {}
+//	std::string getTitle() const override {
+//		return Activity::getTitle() + ", " + std::to_string(year_);
+//	}
+//};
 
 class ActivityList {
 	std::vector<Activity> list;
@@ -28,6 +37,12 @@ public:
 		for (int i = 0; i < list.size(); i++)
 			if (list.at(i).getTitle() == title) return i;
 		return -1;
+	}
+	unsigned size() const {
+		return list.size();
+	}
+	Activity at(unsigned i) const {
+		return list.at(i);
 	}
 	void add(std::string title) {
 		if (contains(title) == -1) list.push_back(Activity(title));
@@ -40,6 +55,8 @@ public:
 		if (index != -1) list.erase(list.begin() + index);
 	}
 	void print() const {
+		if (!size()) return;
+		std::cout << "Your list:" << std::endl;
 		for (auto const& i : list) {
   			std::cout << i.getTitle() << std::endl;
 		}
@@ -47,79 +64,82 @@ public:
 };
 
 void spin(ActivityList const& al) {
-	
+	if (!al.size()) return;
+	for (int i = 1; i < 20; i++) {
+		Sleep(3 * pow(i, 1.8));
+		std::cout << al.at(rand() % al.size()).getTitle() << std::endl;
+	}
+	std::cout << al.at(rand() % al.size()).getTitle() << " <-" << std::endl;
 }
 
-int menu() {
-	std::cout << "to add:\t\t'a <title>'\nto remove:\t'r <title>'\nto stop:\t's'\n";
+int exec() {
+	std::cout << "to add:\t\t'-a <title>'\nto remove:\t'-r <title>'\nto print:\t'-p'\nto spin:\t'-s'\nto quit:\t'-q'\n";
 
 	ActivityList al;
 	std::string buffer;
 	char c = '\0';
-	bool status = 0; // 0 - нет ввода, 1 - ввод на добавление, 2 - ввод на удаление
-
-	while (std::cin.get(c)) {
-		if (c != '\n') { // запись в буфер
-			buffer.push_back(c);
-			//std::cout << c << " pushed to buffer" << std::endl;
-		}
-		else { // обработка буфера
-			// удаление лишних пробелов
-			if (buffer.empty()) continue;
-			while (buffer.at(0) == ' ') {
-				buffer.erase(buffer.begin());
-				//std::cout << "' ' deleted from buffer" << std::endl;
+	bool run_status = true;
+	do {
+		while (std::cin.get(c)) {
+			if (c != '\n') { // запись в буфер
+				buffer.push_back(c);
 			}
-			// добавление
-			if (buffer.at(0) == 'a' && buffer.at(1) == ' ') {
-				// удаление команды 'a' из буфера
-				buffer.erase(buffer.begin());
+			else { // обработка буфера
 				// удаление лишних пробелов
+				if (buffer.empty()) continue;
 				while (buffer.at(0) == ' ') {
 					buffer.erase(buffer.begin());
-					//std::cout << "' ' deleted from buffer" << std::endl;
 				}
-				std::cout << "Command: add (" << buffer << ")" << std::endl;
-				al.add(buffer);
-				//std::cout << buffer << " added to al" << std::endl;
-				buffer.clear();
-				//std::cout << "buffer cleared" << std::endl;
-			}
-			// удаление
-			else if (buffer.at(0) == 'r' && buffer.at(1) == ' ') {
-				buffer.erase(buffer.begin());
-				// удаление лишних пробелов
-				while (buffer.at(0) == ' ') {
-					buffer.erase(buffer.begin());
-					//std::cout << "' ' deleted from buffer" << std::endl;
+				// добавление
+				if (buffer.at(0) == '-' && buffer.at(1) == 'a' && buffer.at(2) == ' ') {
+					buffer.erase(buffer.begin(), buffer.begin() + 2);
+					// удаление лишних пробелов
+					while (buffer.size() && buffer.at(0) == ' ') {
+						buffer.erase(buffer.begin());
+					}
+					std::cout << "Command: add (" << buffer << ")" << std::endl;
+					al.add(buffer);
+					buffer.clear();
 				}
-				std::cout << "Command: remove (" << buffer << ")" << std::endl;
-				al.remove(buffer);
-				//std::cout << buffer << " removed from al" << std::endl;
-				buffer.clear();
-				//std::cout << "buffer cleared" << std::endl;
+				// удаление
+				else if (buffer.at(0) == '-' && buffer.at(1) == 'r' && buffer.at(2) == ' ') {
+					buffer.erase(buffer.begin(), buffer.begin() + 2);
+					// удаление лишних пробелов
+					while (buffer.size() && buffer.at(0) == ' ') {
+						buffer.erase(buffer.begin());
+					}
+					std::cout << "Command: remove (" << buffer << ")" << std::endl;
+					al.remove(buffer);
+					buffer.clear();
+				}
+				// вывод списка
+				else if (buffer.at(0) == '-' && buffer.at(1) == 'p') {
+					buffer.erase(buffer.begin(), buffer.begin() + 2);
+					std::cout << "Command: print" << std::endl;
+					al.print();
+				}
+				// вращение 
+				else if (buffer.at(0) == '-' && buffer.at(1) == 's') {
+					buffer.erase(buffer.begin(), buffer.begin() + 2);
+					std::cout << "Command: spin" << std::endl;
+					spin(al);
+				}
+				// выход
+				else if (buffer.at(0) == '-' && buffer.at(1) == 'q') {
+					buffer.erase(buffer.begin(), buffer.begin() + 2);
+					std::cout << "Command: quit" << std::endl;
+					run_status = false;
+					break;
+				}
+				// не является командой
+				else buffer.clear();
 			}
-			// остановка
-			else if (buffer.at(0) == 's') {
-				std::cout << "Command: stop" << std::endl;
-				break;
-			}
-			else {
-				//std::cout << "wrong command!" << std::endl;
-				buffer.clear();
-				//std::cout << "buffer cleared" << std::endl;
-			}
-
 		}
-	}
-	al.print();
+	} while (run_status);
 	return 0;
 }
 
-int exec() {
-	return menu();
-}
-
 int main() {
+	srand(time(0));
 	return exec();
 }
